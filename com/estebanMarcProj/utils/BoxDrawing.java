b86d266b88c602d6db8d02370bf0499d229328af
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2022-02-25 10:51:39 +0100

    BoxDrawing Definitivo

--- com/estebanMarcProj/utils/BoxDrawing.java
+++ com/estebanMarcProj/utils/BoxDrawing.java
@@ -3,5 +3,14 @@ package com.estebanMarcProj.utils;
 public class BoxDrawing{
     public static final String HORITZONTAL = "\u2500";
     public static final String VERTICAL = "\u2502";
+    public static final String ESQUINA_TOP_L = "\u250C";
+    public static final String ESQUINA_TOP_R = "\u2510";
+    public static final String ESQUINA_BOT_L = "\u2514";
+    public static final String ESQUINA_BOT_R = "\u2518";
+    public static final String LEFT_T = "\u2524";
+    public static final String RIGHT_T = "\u251C";
+    public static final String DOWN_T = "\u252C";
+    public static final String TOP_T = "\u2534";
+    public static final String CROSS = "\u253C";
 }
 
